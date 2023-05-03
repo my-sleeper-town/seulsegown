5c9fa001cd9c1dfa675b58342bb09c3c1301b3c6
이수영 <suyoung@isuyeong-ui-MacBookAir.local>
2023-05-03 12:58:46 +0900

    save : save jumpo info to the db

--- crawler.py
+++ crawler.py
@@ -4,9 +4,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
-#import csv
+import django
+import os
 
-def crawling():
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seulsegown.settings")
+django.setup()
+from store.models import Category, Brand, Jumpo
+
+def Crawling():
+    '''
+    서울특별시에 위치한 ministop 편의점 점포의 정보를 웹 크롤링한 함수입니다.
+    
+    '''
+    '''0. 미니스톱 사이트 접속하기'''
+    jumpos_info = []
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.ministop.co.kr/MiniStopHomePage/page/index.do")
     driver.implicitly_wait(0.5)
@@ -14,55 +26,55 @@ def crawling():
 
     ''' 1. 매장찾기로 마우스 이벤트 '''
     nav_menu = driver.find_element(By.XPATH, '//*[@id="header"]/div[3]/ul/li[1]/a')
-    # button = driver.find_element(By.CLASS_NAME, "nav001")
-    # ActionChains(driver).click(button).perform()
-
-    # 해결 1(성공)
     driver.execute_script("arguments[0].click();", nav_menu)
 
-    # 해결 2(실패)
-    # ActionChains(driver).move_to_element(button).click().perform()
 
     '''2. 드롭다움 박스 선택하기'''
     dropdown = Select(driver.find_element(By.XPATH, '//*[@id="area1"]'))
     dropdown.select_by_value("1")
     driver.implicitly_wait(3)
-    # 검색 버튼 클릭
+
+    '''3. 검색 버튼 클릭'''
     btn = driver.find_element(By.XPATH, '//*[@id="section"]/div[3]/div/div[2]/div[2]/div[1]/a')
     driver.execute_script("arguments[0].click();", btn)
 
 
-    '''3. 가져온 텍스트 파일을 csv파일에 저장'''
-
-    # csv 파일 생성
-    #filename = '/Users/suyoung/Desktop/output.csv'
-    #with open(filename, 'w', encoding='utf-8', newline='') as f:
-    #    writer = csv.writer(f)
-    #    writer.writerow(['name', 'address', 'phone'])
-
-    #cate = Category(category_name='편의점').save()
-    #brd = Brand(brand_id = )
-
-    test_list = [] 
+    '''3. 각 점포 정보 저장하기''' 
     for i in range(1, 261):
         li_element = driver.find_element(By.XPATH, f'//*[@id="section"]/div[3]/div/div[2]/div[2]/div[1]/ul/li[{i}]')
         li_lines = li_element.text.split('\n')
         name = li_lines[0]
         address = li_lines[1]
-        phone = li_lines[2]
+                            
+                    
+        jumpos_info.append({
+            'jumpo_name': name, 
+            'street_address' : address, 
+            'latitude': 37.564214,
+            'longitude': 127.001699,}
+            )
+        
+    return jumpos_info
 
-        print(name)
-        print(address)
-        print(phone)
 
-        # csv 파일에 추가
-        #writer.writerow([name, address, phone])
+def Save_data(jumpos_info, category='편의점', brand='MINISTOP'):
+    category = Category.objects.get_or_create(category_name=category)[0]
+    brand = Brand.objects.get_or_create(category=category, brand_name=brand)[0]
+    success = fail = 0
 
+    for jumpo in jumpos_info:
+        try:
+            Jumpo.objects.create(brand=brand, **jumpo)
+            success += 1
+        except django.db.utils.IntegrityError:
+            print(f'>>> 이미 저장된 데이터({jumpo["jumpo_name"]})가 있습니다.')
+            fail += 1
+            pass
 
+    return success, fail, True
 
 def main():
-    crawling()
-
+    Save_data(Crawling())
 
 if __name__ == "__main__":
     main()
---
+++ store/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='업종명')
+
+    def __str__(self):
+        return self.category_name
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=100, verbose_name='브랜드명')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.brand_name
+
+
+class Jumpo(models.Model):
+    jumpo_name = models.CharField(max_length=50, verbose_name='점포명')
+    street_address = models.CharField(max_length=200, verbose_name='도로명주소')
+    latitude = models.FloatField(blank=True, verbose_name='위도')
+    longitude = models.FloatField(blank=True, verbose_name='경도')
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.jumpo_name} - {self.street_address}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['jumpo_name', 'street_address'], name='jumpo_address'),
+        ]       

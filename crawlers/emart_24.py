10428ba02aad5adae32fdc240376618552481652
sumin25 <sssumin98@daum.net>
2023-05-03 17:30:05 +0900

    impl: edit emart24 crawler

--- crawlers/emart_24.py
+++ crawlers/emart_24.py
@@ -1,6 +1,6 @@
 """
 Retrieves the address and jumpo name of emart 24 stores in Seoul from emart24.co.kr.
-@returns - a list of pairs of (jumpo_name, street_address)
+@returns - a list of dictionary of {jumpo_name, street_address,  latitude, longtitude}
 """
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -10,7 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
 
-def crawl():
+def crawl_emart_24():
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
     driver.get("https://www.emart24.co.kr/store")
     driver.implicitly_wait(0.5)
@@ -29,7 +29,7 @@ def crawl():
     searchBtn = driver.find_element(By.CLASS_NAME, 'searchBtn')
     ActionChains(driver).click(searchBtn).perform()
 
-    loc_list =[] #(jumpo_name, street_address)
+    loc_list =[] #{jumpo_name, street_address, latitude, longtitude}
     nextBtn = driver.find_element(By.CLASS_NAME, 'next')    #하단 다음페이지 버튼
     idx = driver.find_element(By.CLASS_NAME, 'pIndex.focus')    #현재 페이지    
     prev = int(idx.text)
@@ -44,7 +44,26 @@ def crawl():
             time.sleep(1)
 
             for street_address, jumpo_name in zip(address_list, jumpoName_list):
-                loc_list.append((jumpo_name.text, street_address.text))
+
+                lat = ''
+                lon = ''
+                latlon_address = addr_to_lat_lon(street_address.text)
+                if latlon_address is not None:
+                    try:
+                        lon = latlon_address[0]
+                        lat = latlon_address[1]
+                    except IndexError as e:
+                        print(f"Error occurred while extracting latitude and longitude from address {street_address.text}: {e}")
+                        pass
+
+                loc_list.append(
+                            {
+                                'jumpo_name':jumpo_name.text,
+                                'street_address':street_address.text,
+                                'latitude': lat, # 위도 
+                                'longitude': lon  # 경도
+                            }
+                            )
             ActionChains(driver).click(nextBtn).perform()
             time.sleep(1)
 

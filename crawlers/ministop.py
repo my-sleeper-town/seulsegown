7a437874691366ed307bdc6109850473d85e4081
An Nyeong <me@annyeong.me>
2023-05-04 17:12:41 +0900

    fix:missing import from ministop crawler

--- crawlers/ministop.py
+++ crawlers/ministop.py
@@ -4,9 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
-import django
-import os
-
+from utils.utils import addr_to_lat_lng
 
 def crawl_ministop():
     '''
